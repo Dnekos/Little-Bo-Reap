c2fec1c3d85a55a28ae7dccbe4c17f58146c84f8
Carmen Murphy <carmen.murphy@mymail.champlain.edu>
2022-10-03 16:37:50 -0400

    adding minor UIBlockout work

--- Assets/Scripts/UIScripts/ButtonFunctionScript.cs
+++ Assets/Scripts/UIScripts/ButtonFunctionScript.cs
@@ -3,10 +3,13 @@ using UnityEngine.UI;
 using System.Collections;
 public class ButtonFunctionScript : MonoBehaviour
 {
-	public Button[] TreeButtons;
+	public Button[] TreeOneButtons;
+	public Button[] TreeTwoButtons;
+	public Button[] TreeThreeButtons;
 	public GameObject[] MenuCanvases;
 
-	void Start () {
+	void Start () 
+	{
 		
 	}
 
@@ -37,16 +40,54 @@ public class ButtonFunctionScript : MonoBehaviour
 		
 	}
 	public void ResetTree(int treeToReset) {
-		for (int i = 0; i < TreeButtons.Length; i++)
+		
+		//resets whichever buttons are put in the tree.
+		switch (treeToReset)
 		{
-			Button btn = TreeButtons[i].GetComponent<Button>();
- 			ColorBlock cb = btn.colors;
- 			cb.normalColor = Color.white;
-			cb.highlightedColor = Color.green;
-			cb.pressedColor = Color.green;
-			cb.selectedColor = Color.white;
-			cb.disabledColor = Color.white;
- 			btn.colors = cb;
+			case 0:
+			for (int i = 0; i < TreeOneButtons.Length; i++)
+			{
+				Button btn = TreeOneButtons[i].GetComponent<Button>();
+ 				ColorBlock cb = btn.colors;
+ 				cb.normalColor = Color.white;
+				cb.highlightedColor = Color.green;
+				cb.pressedColor = Color.green;
+				cb.selectedColor = Color.white;
+				cb.disabledColor = Color.white;
+ 				btn.colors = cb;
+			}
+			break;
+			case 1:
+			for (int i = 0; i < TreeTwoButtons.Length; i++)
+			{
+				Button btn = TreeTwoButtons[i].GetComponent<Button>();
+ 				ColorBlock cb = btn.colors;
+ 				cb.normalColor = Color.white;
+				cb.highlightedColor = Color.green;
+				cb.pressedColor = Color.green;
+				cb.selectedColor = Color.white;
+				cb.disabledColor = Color.white;
+ 				btn.colors = cb;
+			}
+			break;
+			//reset 
+			case 2:
+			for (int i = 0; i < TreeThreeButtons.Length; i++)
+			{
+				Button btn = TreeThreeButtons[i].GetComponent<Button>();
+ 				ColorBlock cb = btn.colors;
+ 				cb.normalColor = Color.white;
+				cb.highlightedColor = Color.green;
+				cb.pressedColor = Color.green;
+				cb.selectedColor = Color.white;
+				cb.disabledColor = Color.white;
+ 				btn.colors = cb;
+			}
+			break;
+			default:
+			Debug.Log("Reset Button Out Of Bounds");
+			break;
 		}
+		
 	}
 }
