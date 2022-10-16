46ff0e0826cea4b1541c3d22896cb766017cafb0
Carmen Murphy <carmen.murphy@mymail.champlain.edu>
2022-10-15 22:56:24 -0400

    collectables now get pulled in by player. Added grab radius to player.

--- Assets/Scripts/Base/Collectable.cs
+++ Assets/Scripts/Base/Collectable.cs
@@ -5,24 +5,42 @@ using UnityEngine;
 public class Collectable : MonoBehaviour
 {
     bool isCollected;
+    bool isPulled;
+    [SerializeField] float attractSpeed;
+    GameObject playerBody;
 
     // Start is called before the first frame update
-    virtual protected void Start()
+    void Start()
     {
-        
+        playerBody = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     virtual protected void OnTriggerEnter(Collider col)
     {
+        if (col.tag == "GrabRadius" && isCollected == false)
+        {
+            isPulled = true;
+            Debug.Log("GrabRadiusTriggered");
+        }
         if (col.tag == "Player" && isCollected == false)
         {
+            isPulled = false;
             isCollected = true;
             Debug.Log("Colliding with player");
             CollectableEffect();
             Destroy(gameObject); //deletes self after being collected by default
         }
         
+    }
+
+    void Update()
+    {
+        if (isPulled == true)
+        {
+            var step = attractSpeed * Time.deltaTime; // calculate distance to move
+            transform.position = Vector3.MoveTowards(transform.position, playerBody.transform.position, step);
+        }
     }
 
     virtual protected void CollectableEffect()
