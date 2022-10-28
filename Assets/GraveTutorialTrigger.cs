6f3375bf1ee9c17d38246ad19e80c0a33a278f3b
Michael Silverio <michael.silverio@mymail.champlain.edu>
2022-10-27 21:00:13 -0400

    tutorial work

--- Assets/GraveTutorialTrigger.cs
+++ Assets/GraveTutorialTrigger.cs
@@ -9,13 +9,36 @@ public class GraveTutorialTrigger : MonoBehaviour
     [TextArea(15, 20)]
     [SerializeField] string newTutorialText;
     [SerializeField] SheepGrave theGrave;
+    [SerializeField] bool spawnsEnemies;
+    [SerializeField] List<GameObject> enemiesToSpawn;
+    [SerializeField] GameObject spawnPoof;
     bool hasChanged = false;
+
 
     private void Update()
     {
         if(!theGrave.canInteract && !hasChanged)
         {
             tutorialText.text = newTutorialText;
+
+            if(spawnsEnemies)
+            {
+                for (int i = 0; i < enemiesToSpawn.Count; i++)
+                {
+                    Instantiate(spawnPoof, enemiesToSpawn[i].transform.position, Quaternion.identity);
+                }
+                Invoke("SpawnEnemies", 1f);
+            }
+
+            hasChanged = true;
+        }
+    }
+
+    private void SpawnEnemies()
+    {
+        for (int i = 0; i < enemiesToSpawn.Count; i++)
+        {
+            enemiesToSpawn[i].SetActive(true);
         }
     }
 }
--- Assets/KillTrigger.cs
+++ Assets/KillTrigger.cs
@@ -8,9 +8,14 @@ public class KillTrigger : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.GetComponent<EnemyAI>()!= null || other.GetComponent<PlayerSheepAI>()!= null)
+        if (other.GetComponent<EnemyAI>() != null || other.GetComponent<PlayerSheepAI>() != null)
         {
             other.GetComponent<Damageable>()?.ForceKill();
+        }
+
+        if (other.GetComponent<PlayerHealth>() != null)
+        {
+            other.GetComponent<Damageable>().TakeDamage(killAttack, transform.forward);
         }
     }
 }
