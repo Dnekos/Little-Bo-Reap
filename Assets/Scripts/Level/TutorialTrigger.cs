3c442623de0cf0c9ddefc34dd27c92c4b2253b76
DEMETRIUS NEKOS <dnekos2462@gmail.com>
2022-11-12 10:27:55 -0500

    big guy eye flash position adjusted; toggling UI disables tutorial prompts; tutorial prompts on same camera culling layer as player; fixed trigger bounds on tutorial first bell

--- Assets/Scripts/Level/TutorialTrigger.cs
+++ Assets/Scripts/Level/TutorialTrigger.cs
@@ -7,23 +7,40 @@ public class TutorialTrigger : MonoBehaviour
     [SerializeField] GameObject tutorialCanvas;
     [SerializeField] bool enablesPanel;
     [SerializeField] GameObject panelToEnable;
-
+	[SerializeField] GameEvent UIToggle;
+	bool UIOff = false;
     private void Awake()
     {
         tutorialCanvas.SetActive(false);
-    }
+		UIToggle.listener.AddListener(delegate { Toggle(); });
+
+	}
+
+	void Toggle()
+	{
+		UIOff = !UIOff;
+		if (UIOff)
+		{
+			tutorialCanvas.SetActive(false);
+			//if (enablesPanel)
+			//	panelToEnable.SetActive(false);
+
+		}
+	}
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && !UIOff)
         {
             tutorialCanvas.SetActive(true);
-            if (enablesPanel) panelToEnable.SetActive(true);
+            if (enablesPanel)
+				panelToEnable.SetActive(true);
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.CompareTag("Player")) tutorialCanvas.SetActive(false);
+        if (other.CompareTag("Player"))
+			tutorialCanvas.SetActive(false);
     }
 }
