18377ad35e2e9d05f05355c3640444092bfe7456
Demetrius Nekos <dnekos2462@gmail.com>
2022-12-05 15:28:15 -0500

    Line of sight node

---
+++ Assets/Scripts/Enemy/Enemy Graph/StateMachine/Nodes/RaycastNode.cs
@@ -0,0 +1,29 @@
+﻿	using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace XNode.Examples.StateGraph {
+	public class RaycastNode : StateNode
+	{
+		[Input] public float range;
+		[Input] public LayerMask mask;
+		[Input] public Vector3 end;
+
+		[Output] RaycastHit hitInfo;
+
+		public override bool Evaluate()
+		{
+			Vector3 start = (graph as StateGraph).currentUser.transform.position;
+			return Physics.Raycast(start, end - start, range, mask);
+		}
+
+		public override object GetValue(NodePort port)
+		{
+			Vector3 start = (graph as StateGraph).currentUser.transform.position;
+			Physics.Raycast(start, end - start, out hitInfo, range, mask);
+
+			return hitInfo;
+		}
+	}
+}
