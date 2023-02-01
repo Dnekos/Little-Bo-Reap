02b1f28c34b031bee14ac323500a951bd7342cdf
Mitko Ivanov <mitko.ivanov@mymail.champlain.edu>
2023-01-31 21:10:53 -0500

    Made good progress on Frog Enemy prototype  - Frog explodes when near player  - I used the shock wave attack from the "Big guy" and just increased the damage and size
    (Animations need some work, right now there is a bug where when the frog moves, it grows to the size of the "big guy")
    
    The one thing I want to keep working on is the "hop" movement, as currently it is basically just moving like any other enemy

---
+++ Assets/Scripts/Enemy/FrogAI.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class FrogAI : EnemyAI
+{
+	[Header("Explosion")]//this is our explosion
+	[SerializeField] Transform ExplosionSpawnPoint;
+	[SerializeField] float ExplosionDamage;
+
+	// Start is called before the first frame update
+	override protected void Start()
+	{
+		base.Start();
+	}
+
+	// for animation trigger
+	public void SpawnShockwave()
+	{
+		if (activeAttack != null)
+		{
+			activeAttack.SpawnObject(ExplosionSpawnPoint.position);
+			activeAttack.damage = ExplosionDamage;
+		}
+
+		/*
+		We still need to add explosion animation
+		*/
+
+		OnDeath();//kills enemy
+	}
+}
