6bdaa3cbe47eb5457732d88b8c9a063d6ef27873
Demetrius Nekos <dnekos2462@gmail.com>
2023-02-27 17:36:13 -0500

    Bo Peep turns to skulls

--- Assets/Scripts/Camera/CameraOffsetAdjuster.cs
+++ Assets/Scripts/Camera/CameraOffsetAdjuster.cs
@@ -8,13 +8,53 @@ using UnityEngine;
 public class CameraOffsetAdjuster : MonoBehaviour
 {
 	public Vector3 Offset;
+	[Header("Targets")]
 	public Transform LookTarget;
+	[SerializeField] Transform HeadBone;
+	[Header("Conditions")]
+	[SerializeField] float constraint = 120;
+	[SerializeField] float inSpeed = 2, outSpeed = 4;
+	
+	// local vars
+	float lerptime = 0;
+	Quaternion targetRot; // save last rotation to lerp out
+
+	// components
 	Animator anim;
+
 	private void Start()
 	{
 		anim = GetComponent<Animator>();
+		targetRot = HeadBone.rotation;
 	}
 
+	private void LateUpdate()
+	{
+		if (LookTarget != null && HeadBone != null)
+		{
+			Vector3 toTarget = HeadBone.position - LookTarget.position;
+
+			if (Vector3.Angle(HeadBone.forward, toTarget) <= constraint)
+			{
+				targetRot = Quaternion.LookRotation(toTarget, -transform.right);
+				lerptime += Time.deltaTime * inSpeed;
+
+				HeadBone.rotation = Quaternion.Slerp(HeadBone.rotation, targetRot, lerptime);
+			}
+
+		}
+		else if (lerptime > 0)
+		{
+			// lerp back to normal
+			lerptime -= Time.deltaTime * outSpeed;
+			HeadBone.rotation = Quaternion.Slerp(HeadBone.rotation, targetRot, lerptime);
+		}
+
+		// keep it clamped
+		Mathf.Clamp01(lerptime);
+	}
+	/*
+	 * doesnt work with a generic rig, so sad
 	private void OnAnimatorIK(int layerIndex)
 	{
 		Debug.Log("checking");
@@ -27,5 +67,5 @@ public class CameraOffsetAdjuster : MonoBehaviour
 		else
 			anim.SetLookAtWeight(0);
 
-	}
+	}*/
 }
