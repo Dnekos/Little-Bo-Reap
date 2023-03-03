79764c42b5f45202ae4628ab0bdb86f0952d6b1d
Michael Silverio <michael.silverio@mymail.champlain.edu>
2023-03-03 13:38:09 -0500

    pinwheels updated and fixed

--- Assets/Scripts/Sheep/Pinwheel.cs
+++ Assets/Scripts/Sheep/Pinwheel.cs
@@ -6,6 +6,8 @@ public class Pinwheel : MonoBehaviour
 {
     [SerializeField] ParticleSystem tempParticles;
     [SerializeField] float spinTime = 5f;
+    [SerializeField] string idleAnim;
+    [SerializeField] string spinAnim;
     public bool isSpinning;
 
     public IEnumerator SpinPinwheel()
@@ -14,9 +16,11 @@ public class Pinwheel : MonoBehaviour
         {
             isSpinning = true;
             tempParticles.Play(true);
+            GetComponent<Animator>().Play(spinAnim);
             yield return new WaitForSeconds(spinTime);
             tempParticles.Stop(true);
             isSpinning = false;
+            GetComponent<Animator>().Play(idleAnim);
         }
         else yield return null;
     }
