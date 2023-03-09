5d37cde753e251703412b3aec759c00114e54713
Zachary Schwab <zachary.schwab@mymail.champlain.edu>
2023-03-09 13:20:30 -0500

    michael an absolute hero with his just make it move really fast suggestion

--- Assets/Scripts/Player/BoPeepCharge.cs
+++ Assets/Scripts/Player/BoPeepCharge.cs
@@ -2,29 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.InputSystem;
 
 public class BoPeepCharge : MonoBehaviour
 {
 	[Header("Stampeding")]
 	[SerializeField] float chargeSpeed = 35f;
-	[SerializeField] float chargePointRadius = 10f;
-	[SerializeField] float chargeStopDistance = 0f;
-	[SerializeField] float destinationDist = 20f;
+	//[SerializeField] float chargePointRadius = 10f;
+	//[SerializeField] float chargeStopDistance = 0f;
+	//[SerializeField] float destinationDist = 20f;
 
 	[Header("End Condition")]
-	[SerializeField] float chargeCheckTime = 1f;
-	[SerializeField] float chargeCheckSpeed = 2f;
+	[SerializeField] float chargeDuration = 1.5f;
 
-	[Header("Attack and Effects")]
-	[SerializeField] SheepAttack chargeAttack;
-	[SerializeField] GameObject chargeExplosion;
+	[SerializeField] PlayerMovement moveController;
+	bool charging;
 
-	[SerializeField] NavMeshAgent agent;
-	[SerializeField] Rigidbody rb;
+	//hold reg max move speed to set it back after.
+	float maxMoveSpeedTemp;
 
-	[SerializeField] bool charging;
-
-	private void FixedUpdate()
+	private void Update()
 	{
 		if(charging)
 		{
@@ -34,6 +31,10 @@ public class BoPeepCharge : MonoBehaviour
 
 	public void AbilityUpdate()
 	{
+		Vector2 MovementVector = new Vector2(0, 1);
+		moveController.SetMovementVector(MovementVector);
+		//DEPRECATED
+		/*
 		//set destination
 		//get random point inside radius
 		Vector3 chargePosition = this.transform.position + this.transform.forward * destinationDist;
@@ -55,15 +56,24 @@ public class BoPeepCharge : MonoBehaviour
 			// end charge
 			End();
 		}
+		*/
 	}
 
 	public void Begin(Vector3 targettedPos)
 	{
-		agent.enabled = true;
-		rb.isKinematic = true;
+		Debug.Log("triggering bo peep charge");
 		charging = true;
 		//set timer to 0
 		this.StartCoroutine(ChargeTimer());
+
+		Vector2 MovementVector = new Vector2(0, 1);
+  		maxMoveSpeedTemp = GetComponent<PlayerMovement>().GetMaxMoveSpeed();
+		moveController.SetMaxMoveSpeed(chargeSpeed);
+		moveController.SetMovementVector(MovementVector);
+		GetComponent<PlayerInput>().SwitchCurrentActionMap("RamCharge");
+		/*
+		rb.isKinematic = true;
+		agent.enabled = true;
 
 		//set destination
 		//targettedPos.y = this.transform.position.y;
@@ -74,19 +84,28 @@ public class BoPeepCharge : MonoBehaviour
 		agent.speed = chargeSpeed;
 
 		agent.obstacleAvoidanceType = ObstacleAvoidanceType.NoObstacleAvoidance;		
+		*/
 	}
 
 	IEnumerator ChargeTimer()
 	{
-		yield return new WaitForSeconds(chargeCheckTime);
-		if (charging && agent.velocity.magnitude <= chargeCheckSpeed)
+		yield return new WaitForSeconds(chargeDuration);
+		if (charging)
 			End();
 	}
 
 	void End()
 	{
+		GetComponent<PlayerInput>().SwitchCurrentActionMap("PlayerMovement");
+		moveController.SetMovementVector(Vector2.zero);
+		moveController.SetMaxMoveSpeed(maxMoveSpeedTemp);
 		charging = false;
-		agent.enabled = false;
-		rb.isKinematic = false;
+		//agent.enabled = false;
+		//rb.isKinematic = false;
+	}
+	public void OnStopCharge(InputAction.CallbackContext context)
+	{
+		End();
+		moveController.OnJump(context);
 	}
 }
