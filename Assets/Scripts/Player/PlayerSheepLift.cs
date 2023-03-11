e927241659aa565affd2f4c3e23463385a138c6d
Ava Udelson <ava.udelson@mymail.champlain.edu>
2023-03-11 01:15:48 -0500

    Revert "Added checkpoint., souls, end of level, and fail to stakc"
    This reverts commit 3348631e1f6fed2e8ac9ffb42bb48dd18441f66f.

--- Assets/Scripts/Player/PlayerSheepLift.cs
+++ Assets/Scripts/Player/PlayerSheepLift.cs
@@ -12,7 +12,6 @@ public class PlayerSheepLift : MonoBehaviour
 	float sheepHeight;
 
 	[SerializeField] FMODUnity.EventReference placeSound;
-	[SerializeField] FMODUnity.EventReference fail;
 
 	[SerializeField] float SheepLerpSpeed = 0.5f;
 
@@ -69,13 +68,10 @@ public class PlayerSheepLift : MonoBehaviour
 
 		Debug.DrawLine(transform.position, info.point, Color.red, 3);
 		Debug.Log("sheep lift attempt at height "+ (info.distance * SheepSpacingMod) +", estimated sheep height is "+ (sheepHeight * flocks.GetActiveSheep(SheepTypes.BUILD).Count));
-
+		
 		// not enough sheep to make the ladder
 		if (!hitGround || info.distance * SheepSpacingMod > sheepHeight * flocks.GetActiveSheep(SheepTypes.BUILD).Count || info.collider.CompareTag("Sheep") || info.collider.gameObject == platform)
-		{
-			//FMODUnity.RuntimeManager.PlayOneShot(fail, player.transform.position); //sound plays when sheep fail to stack, needs VFX in order to make sense.
 			return false;
-		}
 
 
 		shouldCollapseTower = false;
