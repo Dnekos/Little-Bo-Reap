dea3710370aaaf2244ca28f9cbe804dda72e031c
Ava Udelson <ava.udelson@mymail.champlain.edu>
2023-04-03 04:16:52 -0400

    Soul Sound quieter, collectable sounds, Mushrooms Sounds
    Former-commit-id: f320d379d2b2df70c1041eba11b34cbe1f8eed68

--- Assets/Scripts/Player/BouncyMushroom.cs
+++ Assets/Scripts/Player/BouncyMushroom.cs
@@ -9,6 +9,7 @@ public class BouncyMushroom : MonoBehaviour
     [SerializeField] string bounceAnim;
     [SerializeField] ParticleSystem bounceParticles;
     bool canBounce = true;
+    [SerializeField] FMODUnity.EventReference bounceSFX;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -18,6 +19,7 @@ public class BouncyMushroom : MonoBehaviour
             //effects!
             GetComponent<Animator>().Play(bounceAnim);
             bounceParticles.Play(true);
+            FMODUnity.RuntimeManager.PlayOneShot(bounceSFX,other.transform.position);
 
             //zero out velocity, then add force!
             //velocity must be zeroed out otherwise you might not get any movement if your already coming down fast
