a1f8037f5384a6054f5a44dfa7611a49aad1f018
Mitko Ivanov <mitko.ivanov@mymail.champlain.edu>
2023-04-06 01:08:12 -0400

    Implementation and Bug fixing  - Implemented boss fire breath vfx  - added a node to boss behavior tree that stops boss from attacking while it is currently attacking or moving
    Former-commit-id: 00ae31d7d09c38e4c01ee9716cc39ba8ca0f3a3e

---
+++ Assets/Scripts/Enemy/Enemy Graph/Nodes/BossIsAttacking.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace XNode.Examples.StateGraph
+{
+	public class BossIsAttacking : StateNode
+	{
+		[InspectorName("!="), Input] public bool not;
+
+		public override bool Evaluate()
+		{
+			Animator anim = (graph as StateGraph).currentUser.GetAnimator();
+
+			// name is the attack ones and are in the middle of the animation
+			bool result = ((anim.GetCurrentAnimatorStateInfo(0).IsName("Baba_Yagas_House_Move") || 
+							anim.GetCurrentAnimatorStateInfo(0).IsName("Boss_Ranged_Attack")    ||
+							anim.GetCurrentAnimatorStateInfo(0).IsName("Boss_Flamethrower")     ||
+							anim.GetCurrentAnimatorStateInfo(0).IsName("Boss_Stomp")) &&
+				anim.GetCurrentAnimatorStateInfo(0).normalizedTime < 1.0f);
+
+			return result != not;
+		}
+	}
+}
