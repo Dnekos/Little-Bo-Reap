4dc0f70f5f5123042c243405a144bd4a8e9fe5db
Brigham Boice <brigham.boice@mymail.champlain.edu>
2023-04-11 02:58:45 -0400

    Progression, Pause, and Options UI updates

---
+++ Assets/ClickToggle.cs
@@ -0,0 +1,79 @@
+using Newtonsoft.Json.Schema;
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class ClickToggle : MonoBehaviour
+{
+	[SerializeField] Image iconImage;
+	[SerializeField] GameObject iconCheck;
+	[SerializeField] Color greyedColor;
+	[SerializeField] ButtonState buttonState;
+	[SerializeField] ButtonType buttonType;
+	public StateChangeEvent onStateChanged;
+
+	private void Start()
+	{
+		SetState(buttonState, false);
+	}
+
+	public void SetState(ButtonState buttonState, bool sendMessage)
+	{
+		this.buttonState = buttonState;
+
+		iconImage.color = Color.white;
+		iconCheck.SetActive(false);
+		if (buttonState == ButtonState.Locked) iconImage.color = Color.gray;
+		if (buttonState == ButtonState.Enabled) iconCheck.SetActive(true);
+
+		if (sendMessage) onStateChanged.Invoke(buttonState, buttonType);
+	}
+
+	public void OnButtonClick()
+	{
+		if (buttonState == ButtonState.Disabled)
+		{
+			SetState(ButtonState.Enabled, true);
+		}
+		else
+		{
+			if (buttonState == ButtonState.Enabled)
+			{
+				SetState(ButtonState.Disabled, true);
+			}
+			else
+			{
+				SetState(ButtonState.Enabled, true);
+			}
+		}
+	}
+}
+
+[System.Serializable]
+public class StateChangeEvent : UnityEvent<ButtonState, ButtonType> { }
+
+public enum ButtonState
+{
+	Locked,
+	Disabled,
+	Enabled
+}
+
+public enum ButtonType
+{
+	RingingBell,
+	Momentum,
+	Corruption,
+	Cohesion,
+
+	RidingRampage,
+	Hardened,
+	Sharpness,
+	Forcefullness,
+
+	BlackHole,
+	Heartiness,
+	Stalwart,
+	Sustainability
+}
