350775b083b7e0d212d5a2315eecc1db93d402d8
Michael Silverio <michael.silverio@mymail.champlain.edu>
2023-04-18 18:55:08 -0400

    timeline fixes, sound stuff, kios mode kinda
    Former-commit-id: 1c88bd0eb455af67250be348f73c2eebada6b55c

--- Assets/Scripts/KioskMode.cs
+++ Assets/Scripts/KioskMode.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class KioskMode : MonoBehaviour
 {
     [SerializeField] float timeBeforeCamSpin = 10f;
+    [SerializeField] float timeToHideUI = 60f;
+    [SerializeField] GameObject UI;
     [SerializeField] float camSpinRate = 10f;
+    float currentTime;
+    bool uiHidden;
     bool isSpinning = false;
     float yRot = 0;
 
@@ -21,6 +26,35 @@ public class KioskMode : MonoBehaviour
         {
             yRot += (camSpinRate * Time.deltaTime);
             transform.localRotation = Quaternion.Euler(transform.localRotation.x, yRot, transform.localRotation.z);
+        }
+
+        //kiosk mode crap
+        currentTime += Time.deltaTime;
+        if(currentTime >= timeToHideUI && !uiHidden)
+        {
+            uiHidden = true;
+            UI.SetActive(false);
+        }
+
+
+        if ((Keyboard.current != null && Keyboard.current.anyKey.isPressed) || (Gamepad.current != null && Gamepad.current.leftStick.IsActuated()))
+        {
+            if(uiHidden)
+            {
+                uiHidden = false;
+                UI.SetActive(true);
+            }
+            currentTime = 0f;
+        }
+    }
+
+    public void OnMouseMove(InputAction.CallbackContext context)
+    {
+        currentTime = 0f;
+        if(uiHidden)
+        {
+            uiHidden = false;
+            UI.SetActive(false);
         }
     }
 
