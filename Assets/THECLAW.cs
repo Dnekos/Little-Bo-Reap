310839d5a27e935459186fef8e4a314fa7801e61
Carmen Murphy <carmen.murphy@mymail.champlain.edu>
2023-04-27 15:56:49 -0400

    The End
    Former-commit-id: 8153776540f3e139fe4c3a4a1e2b9e4b68a8e468

--- Assets/THECLAW.cs
+++ Assets/THECLAW.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class THECLAW : MonoBehaviour
 {
 
     [SerializeField] Image image;
+    [SerializeField] TextMeshProUGUI theText;
     [SerializeField] float alphaScale;
     [SerializeField] DialogBox DB;
     // Start is called before the first frame update
@@ -19,6 +21,7 @@ public class THECLAW : MonoBehaviour
     void Update()
     {
         image.color = new Color(image.color.r, image.color.g, image.color.b, (image.color.a + alphaScale));
+        theText.color = new Color(theText.color.r, theText.color.g, theText.color.b, (theText.color.a + alphaScale));
 
         if (image.color.a >= 1) //once THE CLAW is fully visible, allow the game to end
         {
